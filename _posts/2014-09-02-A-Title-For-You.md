a7def1b69453255ff4de5d8d029a09482f5dc18a
koolkalang <diesel92jp90@yahoo.com>
2014-09-02 02:55:02 -0400

    new process and script

---
+++ _posts/2014-09-02-A-Title-For-You.md
@@ -0,0 +1,13 @@
+---
+layout: post
+title: A Title For You
+tags:
+- personal
+- process
+- python
+- art
+---
+I'm doing stuff right now in this file, to do things in this file.
+This is the second line in the content of the file. 
+
+I wonder what else I should be doing here.
---
+++ _posts/make.py
@@ -0,0 +1,35 @@
+import os
+import time
+import re
+
+for filename in os.listdir("."):
+    if filename == 'post':
+        post = open(filename)
+        break
+
+# extract parts
+line = (post.readline()).rstrip('\r\n')
+tags = (post.readline()).rstrip('\r\n')
+date =  time.strftime("%Y-%m-%d-")
+title = re.sub('[ ]', '-', line)
+postname = date + title + ".md"
+
+tags = tags.split( )
+new_tags = ''
+for tag in tags:
+    new_tags = new_tags + '- ' + tag + '\n'
+
+header = "---\nlayout: post\ntitle: " + line + "\ntags:\n" + new_tags + "---\n"
+
+
+file = open(postname, 'w')
+file.write(header)
+while True:
+    buffer=post.readline()
+    if not buffer: break
+    file.write(buffer)
+
+file.write("---")
+file.close()
+
+
